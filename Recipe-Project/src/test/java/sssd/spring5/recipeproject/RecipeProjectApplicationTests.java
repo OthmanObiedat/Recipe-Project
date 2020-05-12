a97dae207cae32494e2fcdf5c126d95cb83efb49
othman.alothman <othman.alothman@2STP-6892L.secondstep.local>
2020-05-12 10:24:53 +0300

    Create the Project.

---
+++ Recipe-Project/src/test/java/sssd/spring5/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sssd.spring5.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
